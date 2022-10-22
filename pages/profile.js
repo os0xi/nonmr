e55e90e97f77b5f92b3b7928e9de177f4dee17d5
0xInu <calin.cretiu@gmail.com>
2022-10-22 15:01:35 +0300

    removed profile page getserversideprops

--- pages/profile.js
+++ pages/profile.js
@@ -1,44 +1,30 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useTheme } from "@emotion/react";
 
 import React from "react";
 
-function Profile({ session }) {
+function Profile() {
   const theme = useTheme();
+  const session = useSession();
+
   return (
     <Box>
       <Typography variant="p" fontWeight={700}>
         {`Address `}
+
         <Typography
           variant="span"
           sx={{ ...theme.customElements.gradientText }}
         >
-          {session.user.address}
+          {session.data.user.address}
         </Typography>
       </Typography>
     </Box>
   );
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  // redirect if not authenticated
-  //handled with Profile.requiresAuth = true && <WithAuth> hoc now
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       destination: "/",
-  //       permanent: false,
-  //     },
-  //   };
-  // }
-
-  return {
-    props: { session: session },
-  };
-}
 Profile.requiresAuth = true;
 
 export default Profile;
